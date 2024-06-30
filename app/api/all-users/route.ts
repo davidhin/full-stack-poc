a59cf57ce0a32932f4a28f70a7b209fe75fc83b9
David Hin <davidhin1b@gmail.com>
2024-06-30 17:55:32 +0930

    Revalidate to fix caching
    Attempt to fix caching
    
    Lower cache time
    
    Revalidate page
    
    Just use revalidate

--- app/api/all-users/route.ts
+++ app/api/all-users/route.ts
@@ -1,5 +1,7 @@
 import prisma from "../../lib/prisma";
 
+export const revalidate = 5;
+
 export async function GET() {
   const users = await prisma.user.findMany();
 
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import prisma from "../lib/prisma";
 
+export const revalidate = 5;
+
 export default async function Posts() {
   const users = await prisma.user.findMany({ take: 10 });
 
