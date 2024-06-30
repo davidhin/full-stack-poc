18916e0d810b25af876b3495b8f8bf00fb17a027
David Hin <davidhin1b@gmail.com>
2024-06-30 20:00:33 +0930

    Create user in table

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,5 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
 import { getServerSession } from "next-auth";
+import prisma from "../lib/prisma";
 
 export const authOptions = {
   providers: [
@@ -8,6 +9,25 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
+  callbacks: {
+    async signIn({ user, account }: any) {
+      const existingUser = await prisma.user.findUnique({
+        where: { email: user.email },
+      });
+
+      if (!existingUser) {
+        await prisma.user.create({
+          data: {
+            email: user.email,
+            name: user.name,
+            ...(account.provider === "google" && { google: user.id }),
+          },
+        });
+      }
+
+      return true;
+    },
+  },
 };
 
 export async function getSession() {
