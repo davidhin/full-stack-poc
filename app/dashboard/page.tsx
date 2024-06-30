fc214f3afdf429fd9b570a3ae8b4faeebe7a3224
David Hin <davidhin1b@gmail.com>
2024-06-30 15:09:42 +0930

    Print users in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,8 +2,17 @@ import Link from "next/link";
 import prisma from "../lib/prisma";
 
 export default async function Posts() {
-  const users = await prisma.user.findMany();
-  console.log(users);
+  const users = await prisma.user.findMany({ take: 10 });
 
-  return <Link href="/">Go Back</Link>;
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{`${user.name} -> ${user.email}`}</li>
+        ))}
+      </ul>
+      <Link href="/">Go Back</Link>
+    </div>
+  );
 }
