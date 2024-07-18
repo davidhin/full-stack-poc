c45325e7983362636283bfe0530f6a0fb596dc6a
David Hin <davidhin1b@gmail.com>
2024-07-18 10:55:04 +0930

    Add onClick to sidebar

--- __tests__/navigation-sidebar.test.tsx
+++ __tests__/navigation-sidebar.test.tsx
@@ -2,18 +2,18 @@ import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import NavigationSidebar from "@/app/components/navigation-sidebar";
 
-test("Page with user session", async () => {
+test("Page with user session", () => {
   const session = {
     user: { name: "test" },
     expires: "2024-07-01T00:00:00.000Z",
   };
-  const page = await NavigationSidebar({ session });
+  const page = <NavigationSidebar session={session} />;
   render(page);
   expect(screen.getByRole("link", { name: "Sign Out" })).toBeDefined();
 });
 
 test("Page without user session", async () => {
-  const page = await NavigationSidebar({ session: null });
+  const page = <NavigationSidebar session={null} />;
   render(page);
   expect(screen.getByRole("link", { name: "Sign In" })).toBeDefined();
 });
--- app/components/navigation-sidebar.tsx
+++ app/components/navigation-sidebar.tsx
@@ -4,14 +4,25 @@ import Button from "@/app/components/button";
 import * as Popover from "@radix-ui/react-popover";
 import { Session } from "next-auth";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function NavigationSidebar({
   session,
 }: {
   session?: Session | null;
 }) {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+    console.log(count);
+  }
+
   return (
-    <div className="bg-red-200 p-8 w-80 flex flex-col h-screen">
+    <div
+      className="bg-red-200 p-8 w-80 flex flex-col h-screen"
+      onClick={handleClick}
+    >
       <Popover.Root>
         <Popover.Trigger>More info</Popover.Trigger>
         <Popover.Portal>
